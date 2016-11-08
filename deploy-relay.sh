35973aa0ee32b611cc2ca57f2cc93336ef337b92
Shulepov Kirill <shulepovk@gmail.com>
2016-11-08 11:04:28 +0300

    change mydest

--- deploy-relay.sh
+++ deploy-relay.sh
@@ -4,6 +4,7 @@ DOMAINNAME=$1
 
 echo $DOMAINNAME relay:$DOMAINNAME >> /etc/postfix/transport
 postconf transport_maps=hash:/etc/postfix/transport
+postconf mydestination=localhost
 
 postalias /etc/postfix/transport
 postfix check && postfix reload
